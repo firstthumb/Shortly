83b28545e158cad0cce0a402bc09a179a679d2e4
firstthumb <kocamanerol@gmail.com>
2019-11-06 22:23:42 +0200

    Response updated

--- server/pkg/handler/shortener.go
+++ server/pkg/handler/shortener.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"io/ioutil"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"shortly/pkg/service"
 )
@@ -15,13 +16,15 @@ type ShortlyRequest struct {
 }
 
 type ShortlyResponse struct {
-	ShortUrl string `json:"short_url"`
+	ShortUrl 	string `json:"short_url"`
+	Url 		string `json:"url"`
 }
 
 func shortenIndex(srv service.ShortenService) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
+			log.Errorf("Shorten Handler => Could not read request. %v", err)
 			http.Error(w, err.Error(), 500)
 			return
 		}
@@ -29,18 +32,21 @@ func shortenIndex(srv service.ShortenService) http.Handler {
 		request := &ShortlyRequest{}
 		err = json.Unmarshal(body, request)
 		if err != nil {
+			log.Errorf("Shorten Handler => Could not unmarshal request. %v", err)
 			http.Error(w, err.Error(), 500)
 			return
 		}
 
 		resp, err := srv.Shorten(service.TIKITOK, request.Link)
 		if err != nil {
+			log.Errorf("Shorten Handler => Could not call shorten service. %v", err)
 			http.Error(w, err.Error(), 500)
 			return
 		}
 
-		output, err := json.Marshal(ShortlyResponse{ShortUrl: resp})
+		output, err := json.Marshal(ShortlyResponse{ShortUrl: resp, Url: request.Link})
 		if err != nil {
+			log.Errorf("Shorten Handler => Could not marshal response. %v", err)
 			http.Error(w, err.Error(), 500)
 			return
 		}
